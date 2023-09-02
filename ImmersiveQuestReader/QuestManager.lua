d93abde4b7238c3fdad6a73d55f0a374a900a028
Lilian Hiault <lilian.hiault@laposte.net>
2023-09-03 00:13:16 +0200

    Correctly import new quest database

--- ImmersiveQuestReader/QuestManager.lua
+++ ImmersiveQuestReader/QuestManager.lua
@@ -6,38 +6,9 @@ import "EsyIQR.ImmersiveQuestReader.QuestDatabase"
 QuestManager = class();
 
 function QuestManager:Constructor()
-    self.DEBUG = false;
+    self.DEBUG = true;
     
-    -- if QUESTS then self.questsByLevel = QUESTS.quest end -- For testing purposes
-    self.questsByLevel = {
-        QUESTS_A.quest,
-        QUESTS_B.quest,
-        QUESTS_C.quest,
-        QUESTS_D.quest,
-        QUESTS_E.quest,
-        QUESTS_F.quest,
-        QUESTS_G.quest,
-        QUESTS_H.quest,
-        QUESTS_I.quest,
-        QUESTS_J.quest,
-        QUESTS_K.quest,
-        QUESTS_L.quest,
-        QUESTS_M.quest,
-        QUESTS_N.quest,
-        QUESTS_O.quest,
-        QUESTS_P.quest,
-        QUESTS_Q.quest,
-        QUESTS_R.quest,
-        QUESTS_S.quest,
-        QUESTS_T.quest,
-        QUESTS_U.quest,
-        QUESTS_V.quest,
-        QUESTS_W.quest,
-        QUESTS_X.quest,
-        QUESTS_Y.quest,
-        QUESTS_Z.quest,
-        QUESTS_OTHER.quest
-    }
+    self.questsByLevel = QUEST_DATABASE
 end
 
 function QuestManager:IsNewQuest(chatMessage)
@@ -67,16 +38,16 @@ end
 
 -- Returns the quest text for a given quest name
 function QuestManager:GetQuestFromName(questName)
+    -- local firstCharacter = string.sub(questName, 1, 1);
     for _, database in pairs(self.questsByLevel) do
         for _, quest in pairs(database) do
-            -- if quest.name then Turbine.Shell.WriteLine("IQR.QuestManager> Quest searched: '" .. quest.name .. "'") end
             if quest.name == questName then
                 if self.DEBUG then Turbine.Shell.WriteLine("IQR.QuestManager> Quest found: '" .. quest.name .. "'") end
-                return quest -- Retourne la quête si le nom correspond
+                return quest -- Return the quest if the name matches
             end
         end
     end
-    return nil -- Retourne nil si la quête n'est pas trouvée
+    return nil -- Return nil if the quest is not found
 end
 
 -- Add the quest text and state to the quest as _text and _state
